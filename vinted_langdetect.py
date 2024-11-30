332cfc5817468ec63585290f33c8773aa02f6307
Hanka Růžičková <hana.forstova@gmail.com>
2024-11-30 15:05:13 +0100

    Update vinted_langdetect.py

--- vinted_langdetect.py
+++ vinted_langdetect.py
@@ -2,8 +2,11 @@
 Working, but dismissed as unreliable. Detects Czech words without diacritics 
 in another language. Indentify "tl" as a country code for East Timor instead of "cs" 
 for the Czech Republic'''
+
 from langdetect import detect
 import seaborn as sns
+
 with open('dataset_vinted-premium-actor_2024-10-22_06-42-43-186.csv', mode='r', encoding='utf-8') as file: df = pd.read_csv(file)
 [def detect_my(file): try: return detect(file) except: return 'unknown']
+  
 df['language'] = df['title'] .apply(detect_my) df.to_csv('updated_vinted_dataset_with_language.csv', index=False)
