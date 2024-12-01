44b93a8a09d186677dfbaca84de7a99e1657d42b
Hanka Růžičková <hana.forstova@gmail.com>
2024-12-01 16:54:51 +0100

    Update vinted_transformation_revised1.py

--- vinted_transformation_revised1.py
+++ vinted_transformation_revised1.py
@@ -3,11 +3,10 @@
 import pandas as pd
 import uuid
 
-# File paths
 input_path = 'in/tables/Vinted_mango_kalhoty_zeny.csv'
 output_path = 'out/tables/vinted_mango_kalhoty_zeny_vystup.csv'
 
-# Define the expected data types for each column
+# Define data types
 dtype = {
     "brand_title": str,
     "favourite_count": str,
@@ -19,10 +18,9 @@ dtype = {
     "url": str,
   	}
 
-# Reading the data from the input CSV file with specified dtypes
 df = pd.read_csv(input_path, dtype=dtype)
 
-# Rename columns to match specified output format
+# Rename columns
 df = df.rename(columns={
     "id": "product_id",
     "size_title": "size",
@@ -35,7 +33,7 @@ df = df.rename(columns={
 # Exclude sizes containing "měsíců", "roky", or numeric age-related sizes like "9 let"
 df = df[~df['size'].str.contains(r'\d+\s?(let|měsíců|roky)', na=False, case=False)]
 
-# Adding new columns with specified default values
+# Adding new columns
 df['brand_title'] = 'Mango'
 df['category'] = 'Ženy'
 df['product'] = 'Kalhoty'
@@ -44,7 +42,7 @@ df['e_shop'] = 'Vinted'
 # Generating a unique UUID for each row in a new column "database_id"
 df['database_id'] = [str(uuid.uuid4()) for _ in range(len(df))]
 
-# Define the final columns to retain
+# Define the final columns
 final_columns = [
     "brand_title", "favourite_count", "product_id",
     "price", "size", "condition", "name", "url", "category", "product",
@@ -55,5 +53,5 @@ final_columns = [
 existing_columns = [col for col in final_columns if col in df.columns]
 df = df[existing_columns]
 
-# Saving the updated DataFrame to the output CSV file
+# Final output
 df.to_csv(output_path, index=False)
